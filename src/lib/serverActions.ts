55689da76abbf5c1ba4fd3fa98ceb8bee41fa986
Nash Pillai <nash.pillai@protonmail.com>
2024-07-11 01:03:12 -0400

    style markdown with tailwind prose

--- src/lib/serverActions.ts
+++ src/lib/serverActions.ts
@@ -4,8 +4,8 @@ export async function getPosts() {
 			title: "The Art of Writing",
 			summary: "A quick guide to writing well",
 			content: `This is _the_ content **of** the post \n
-			# hello world
-			[hi](https://example.com)`,
+# hello world
+[hi](https://example.com)`,
 			slug: "the-art-of-writing",
 			image: "/images/blog/01.jpg",
 			authorImg: "/images/team/01.jpg",
