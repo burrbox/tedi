9e274e50217fb878f79170164f2b033ba8a745fc
Nash Pillai <nash.pillai@protonmail.com>
2024-07-12 04:46:50 -0400

    add discord to signin page

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -147,22 +147,23 @@ export default function SignIn({ searchParams }: { searchParams: Record<string, 
 						<span className="flex-auto pl-3 text-rose-50">Continue With Github</span>
 					</button>
 					<button
-						className="btn-sm relative my-3 flex w-full bg-gray-200 text-sm after:flex-1"
+						className="btn-sm relative my-3 flex w-full bg-gray-800 text-sm after:flex-1"
 						type="button"
-						onClick={() => handleSignIn("azure-ad")}>
+						onClick={() => handleSignIn("discord")}>
 						<div className="flex flex-1 items-center">
-							<svg className="h-6 w-6 shrink-0 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23">
-								<path fill="#f35325" d="M1 1h10v10H1z" />
-								<path fill="#81bc06" d="M12 1h10v10H12z" />
-								<path fill="#05a6f0" d="M1 12h10v10H1z" />
-								<path fill="#ffba08" d="M12 12h10v10H12z" />
+							<svg
+								role="img"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6 shrink-0 fill-current text-gray-300">
+								<path d="M20.317 4.3698a19.7913 19.7913 0 00-4.8851-1.5152.0741.0741 0 00-.0785.0371c-.211.3753-.4447.8648-.6083 1.2495-1.8447-.2762-3.68-.2762-5.4868 0-.1636-.3933-.4058-.8742-.6177-1.2495a.077.077 0 00-.0785-.037 19.7363 19.7363 0 00-4.8852 1.515.0699.0699 0 00-.0321.0277C.5334 9.0458-.319 13.5799.0992 18.0578a.0824.0824 0 00.0312.0561c2.0528 1.5076 4.0413 2.4228 5.9929 3.0294a.0777.0777 0 00.0842-.0276c.4616-.6304.8731-1.2952 1.226-1.9942a.076.076 0 00-.0416-.1057c-.6528-.2476-1.2743-.5495-1.8722-.8923a.077.077 0 01-.0076-.1277c.1258-.0943.2517-.1923.3718-.2914a.0743.0743 0 01.0776-.0105c3.9278 1.7933 8.18 1.7933 12.0614 0a.0739.0739 0 01.0785.0095c.1202.099.246.1981.3728.2924a.077.077 0 01-.0066.1276 12.2986 12.2986 0 01-1.873.8914.0766.0766 0 00-.0407.1067c.3604.698.7719 1.3628 1.225 1.9932a.076.076 0 00.0842.0286c1.961-.6067 3.9495-1.5219 6.0023-3.0294a.077.077 0 00.0313-.0552c.5004-5.177-.8382-9.6739-3.5485-13.6604a.061.061 0 00-.0312-.0286zM8.02 15.3312c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9555-2.4189 2.157-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.9555 2.4189-2.1569 2.4189zm7.9748 0c-1.1825 0-2.1569-1.0857-2.1569-2.419 0-1.3332.9554-2.4189 2.1569-2.4189 1.2108 0 2.1757 1.0952 2.1568 2.419 0 1.3332-.946 2.4189-2.1568 2.4189Z" />
 							</svg>
 						</div>
-						<span className="flex-auto pl-3 text-gray-800">Continue With Microsoft</span>
+						<span className="flex-auto pl-3 text-gray-200">Continue With Discord</span>
 					</button>
 					{/* Privacy Policy */}
 					<div className="flex gap-1 align-middle">
-						<div className="mx-auto mb-1 block font-medium text-slate-400">
+						<div className="mx-auto mb-1 block font-medium text-slate-700">
 							By signing in you agree to our{" "}
 							<Link href="/privacy-policy" className="text-blue-500 underline">
 								Privacy Policy
