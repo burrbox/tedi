09807029f4d527c3cd487e1afa3caa894aa21c86
Nash Pillai <nash.pillai@protonmail.com>
2024-07-12 03:12:01 -0400

    fix other build errors

--- src/lib/serverActions.ts
+++ src/lib/serverActions.ts
@@ -1,5 +1,17 @@
 import { db } from "@/server/db";
 
 export async function getPosts() {
-	return db.post.findMany({ include: { author: true } });
+	return [
+		{
+			title: "The Art of Writing",
+			summary: "A quick guide to writing well",
+			content: `This is _the_ content **of** the post \n
+# hello world
+[hi](https://example.com)`,
+			slug: "the-art-of-writing",
+			image: "/images/blog/01.jpg",
+			author: { name: "John Doe" as string | null, image: "/images/team/01.jpg" as string | null },
+			createdAt: new Date("2022-01-01"),
+		},
+	].concat(await db.post.findMany({ include: { author: true } }));
 }
