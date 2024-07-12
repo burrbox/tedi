45bd75a81db19f6d23892c77e084b492ab6e12e6
Nash Pillai <nash.pillai@protonmail.com>
2024-07-12 19:00:42 -0400

    show sign in button in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 
 const links = [
@@ -28,12 +30,41 @@ export default function Header() {
 					/>
 					<h1 className="text-xl font-bold text-green-700">The Environmental Defense Initiative</h1>
 				</div>
-				<nav className="flex space-x-8 text-lg">
+				<nav className="flex items-center space-x-8 text-lg">
 					{links.map((link) => (
-						<a key={link.name} href={link.href} className={link.href === path ? "text-green-700" : "text-blue-700"}>
+						<Link key={link.name} href={link.href} className={link.href === path ? "text-green-700" : "text-blue-700"}>
 							{link.name}
-						</a>
+						</Link>
 					))}
+					{session?.user ? (
+						<div className="border-gray-700 pb-3 pt-4">
+							<div className="flex items-center px-5">
+								<div className="flex-shrink-0">
+									<Image
+										className="h-10 w-10 rounded-full"
+										src={session?.user.image ?? "https://img.icons8.com/96/FFFFFF/user-male-circle.png"}
+										width={40}
+										height={40}
+										alt=""
+									/>
+								</div>
+								<div className="ml-3">
+									<div className="text-base font-medium text-gray-800">{session?.user.name}</div>
+									<div className="text-sm font-medium text-gray-400">{session?.user.email}</div>
+								</div>
+							</div>
+						</div>
+					) : (
+						<Link className="btn-sm group w-full bg-blue-700 shadow-sm md:w-max" href={`/signin?redirect=${path}`}>
+							{/* <ShimmerButton className="shadow-2xl" background="#4F46E5"> */}
+							<span className="whitespace-pre-wrap from-white to-slate-900/10 px-2 py-1 text-center text-lg font-medium leading-none tracking-tight text-white">
+								Sign In{" "}
+								<span className="ml-1 tracking-normal text-sky-300 transition-transform duration-150 ease-in-out group-hover:translate-x-0.5">
+									-&gt;
+								</span>
+							</span>
+						</Link>
+					)}
 				</nav>
 			</div>
 		</header>
