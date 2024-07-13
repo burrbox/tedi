5bc1ce228b4e164bfa7de4d0b48037892fdef959
HiroDookie <hryk.sakuma@gmail.com>
2024-07-13 22:59:15 +0900

    redid home because first time was weird (its still weird)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
 	return (
 		<div
-			className="relative h-[400px] w-full pb-8"
+			className="relative flex h-dvh w-full items-end"
 			style={{
 				backgroundImage:
 					"url(https://static.wixstatic.com/media/11062b_b21f82750bf3464da0b803cc5304b4e9f000.jpg/v1/fill/w_1587,h_934,al_c,q_85,usm_0.33_1.00_0.00,enc_auto/11062b_b21f82750bf3464da0b803cc5304b4e9f000.jpg)",
@@ -10,77 +10,77 @@ export default async function Home() {
 				width: "100vw",
 				height: "100vh",
 			}}>
-			<div className="container absolute inset-0 mt-20 flex h-1/3 w-2/5 min-w-96 flex-col items-center justify-center rounded-3xl bg-white py-6 opacity-90">
-				<div className="text-wrap text-center text-blue-600">
-					<h1 className="m-8 text-6xl">Save Our Environment</h1>
-					<p className="text-wrap text-xl">
+			<div className="xs:mt-32 absolute inset-0 mx-auto w-fit md:mt-32 lg:mt-60" data-aos="fade-down">
+				<span className="mt-6 flex flex-col place-items-center rounded-xl bg-white opacity-90">
+					<h1 className="m-8 text-wrap text-center text-6xl font-semibold text-blue-600">Save Our Environment</h1>
+					<h2 className="text-wrap py-6 text-center text-3xl text-blue-600">
 						Join our initiative.
 						<br />
 						Help us fight for a cleaner world!
-					</p>
-				</div>
-				<div className="flex w-96 justify-center pb-4 pt-8 text-center">
-					<h2 className="btn w-36 rounded-lg border border-green-500 bg-green-500 py-3 text-white duration-300 ease-in-out hover:bg-white hover:text-green-500">
-						<a href="/join-us">Join Us</a>
 					</h2>
-				</div>
+					<span className="justify-center">
+						<button className="my-6 rounded-xl border-2 border-green-600 bg-green-600 bg-opacity-100 px-10 py-2 text-white text-opacity-100 duration-300 hover:bg-white hover:text-green-600">
+							Join Us
+						</button>
+					</span>
+				</span>
 			</div>
-			<div className="absolute inset-x-0 bottom-0 mx-20 flex flex-row justify-around">
-				<div className="relative flex flex-col bg-green-800 opacity-80">
-					<div className="self-start px-6 py-3">
-						<h1 className="text-wrap text-3xl text-white">About</h1>
-						<h2 className="w-3/4 text-wrap py-4 text-white">{"Learn about TEDI's mission and meet our team."}</h2>
+			<div className="container mx-auto flex flex-row justify-center sm:flex-wrap md:flex-wrap lg:flex-nowrap">
+				<span className="py-auto h-fit flex-row bg-green-800 px-4 opacity-80 sm:w-1/3 md:w-1/3 lg:w-1/5">
+					<h1 className="mt-3 text-3xl text-white text-opacity-100">About</h1>
+					<h2 className="mt-auto text-wrap py-4 text-xl text-white text-opacity-100">
+						Learn about TEDI's mission and meet our team.
+					</h2>
+					<div className="flex items-center justify-center pb-8">
+						<button className="rounded-xl border-2 border-green-600 bg-white px-4 py-2 text-center text-green-600 text-opacity-100 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
+							View More
+						</button>
 					</div>
-					<div className="absolute inset-x-0 bottom-0 flex justify-center py-3">
-						<span className="btn rounded-xl border-green-600 bg-white px-10 py-4 text-green-600 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
-							<a href="/about">View More</a>
-						</span>
+				</span>
+				<span className="py-auto h-fit flex-row bg-lime-800 px-4 opacity-80 sm:w-1/3 md:w-1/3 lg:w-1/5">
+					<h1 className="mt-3 text-3xl text-white text-opacity-100">Blog</h1>
+					<h2 className="mt-auto text-wrap py-4 text-xl text-white text-opacity-100">
+						Read about the latest issues in our environment.
+					</h2>
+					<div className="flex items-center justify-center pb-8">
+						<button className="rounded-xl border-2 border-green-600 bg-white px-4 py-2 text-center text-green-600 text-opacity-100 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
+							View More
+						</button>
 					</div>
-				</div>
-				<div className="relative flex flex-col bg-lime-800 opacity-80">
-					<div className="self-start px-6 py-3">
-						<h1 className="text-wrap text-3xl text-white">Blog</h1>
-						<h2 className="w-3/4 text-wrap pt-4 text-white">Read about the latest issues in our environment.</h2>
+				</span>
+				<span className="py-auto h-fit flex-row bg-green-800 px-4 opacity-80 sm:w-1/3 md:w-1/3 lg:w-1/5">
+					<h1 className="mt-3 text-3xl text-white text-opacity-100">Petitions</h1>
+					<h2 className="mt-auto text-wrap py-4 text-xl text-white text-opacity-100">
+						Sign a petition to advocate for new policies.
+					</h2>
+					<div className="flex items-center justify-center pb-8">
+						<button className="rounded-xl border-2 border-green-600 bg-white px-4 py-2 text-center text-green-600 text-opacity-100 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
+							View More
+						</button>
 					</div>
-					<div className="absolute inset-x-0 bottom-0 flex justify-center py-3">
-						<span className="btn rounded-xl border-green-600 bg-white px-10 py-4 text-green-600 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
-							<a href="/blog">View More</a>
-						</span>
+				</span>
+				<span className="py-auto h-fit flex-row bg-lime-800 px-4 opacity-80 sm:w-1/3 md:w-1/3 lg:w-1/5">
+					<h1 className="mt-3 text-3xl text-white text-opacity-100">Our Work</h1>
+					<h2 className="mt-auto text-wrap py-4 text-xl text-white text-opacity-100">
+						Learn about our upcoming campaigns and events.
+					</h2>
+					<div className="flex items-center justify-center pb-8">
+						<button className="rounded-xl border-2 border-green-600 bg-white px-4 py-2 text-center text-green-600 text-opacity-100 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
+							View More
+						</button>
 					</div>
-				</div>
-				<div className="relative flex flex-col bg-green-800 opacity-80">
-					<div className="self-start px-6 py-3">
-						<h1 className="text-wrap text-3xl text-white">Petitions</h1>
-						<h2 className="w-3/4 text-wrap py-4 text-white">Sign a petition to advocate for new policies.</h2>
+				</span>
+				<span className="py-auto h-fit flex-row bg-green-800 px-4 opacity-80 sm:w-1/3 md:w-1/3 lg:w-1/5">
+					<h1 className="mt-3 text-3xl text-white text-opacity-100">Join Us</h1>
+					<h2 className="mt-auto text-wrap py-4 text-xl text-white text-opacity-100">
+						Become a member or join our team.
+					</h2>
+					<div className="flex items-center justify-center pb-8">
+						<button className="rounded-xl border-2 border-green-600 bg-white px-4 py-2 text-center text-green-600 text-opacity-100 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
+							View More
+						</button>
 					</div>
-					<div className="absolute inset-x-0 bottom-0 flex justify-center py-3">
-						<span className="btn rounded-xl border-green-600 bg-white px-10 py-4 text-green-600 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
-							<a href="/petitions">View More</a>
-						</span>
-					</div>
-				</div>
-				<div className="relative flex flex-col bg-lime-800 opacity-80">
-					<div className="self-start px-6 py-3">
-						<h1 className="text-wrap text-3xl text-white">Our Work</h1>
-						<h2 className="w-3/4 text-wrap py-4 text-white">Learn about our upcoming campaigns and events.</h2>
-					</div>
-					<div className="absolute inset-x-0 bottom-0 flex justify-center py-3">
-						<span className="btn rounded-xl border-green-600 bg-white px-10 py-4 text-green-600 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
-							<a href="/our-work">View More</a>
-						</span>
-					</div>
-				</div>
-				<div className="relative flex flex-col bg-green-800 opacity-80">
-					<div className="self-start px-6 py-3">
-						<h1 className="text-wrap text-3xl text-white">Join Us</h1>
-						<h2 className="w-3/4 text-wrap py-4 text-white">Become a member or join our team.</h2>
-					</div>
-					<div className="absolute inset-x-0 bottom-0 flex justify-center py-3">
-						<span className="btn rounded-xl border-green-600 bg-white px-10 py-4 text-green-600 opacity-100 duration-300 hover:bg-green-600 hover:text-white">
-							<a href="/join-us">View More</a>
-						</span>
-					</div>
-				</div>
+				</span>
 			</div>
 		</div>
 	);
