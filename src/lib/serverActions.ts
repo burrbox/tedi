6ef6d91fe8cf5124d980b7d6a8643590398f415a
Nash Pillai <nash.pillai@protonmail.com>
2024-07-13 04:27:52 -0400

    remove test data from get articles server action

--- src/lib/serverActions.ts
+++ src/lib/serverActions.ts
@@ -6,19 +6,7 @@ import { redirect } from "next/navigation";
 import { z } from "zod";
 
 export async function getPosts() {
-	return [
-		{
-			title: "The Art of Writing",
-			summary: "A quick guide to writing well",
-			content: `This is _the_ content **of** the post \n
-# hello world
-[hi](https://example.com)`,
-			slug: "the-art-of-writing",
-			image: "/images/blog/01.jpg",
-			author: { name: "John Doe" as string | null, image: "/images/team/01.jpg" as string | null },
-			createdAt: new Date("2022-01-01"),
-		},
-	].concat(await db.post.findMany({ include: { author: true } }));
+	return db.post.findMany({ include: { author: true } });
 }
 
 export async function addEmailSubscription(email: string) {
