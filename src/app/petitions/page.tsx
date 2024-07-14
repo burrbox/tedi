85091f275a2e40b77abe7a6e01070fe0aa7247d3
HiroDookie <hryk.sakuma@gmail.com>
2024-07-14 11:25:31 +0900

    petitions on tablet

--- src/app/petitions/page.tsx
+++ src/app/petitions/page.tsx
@@ -11,8 +11,8 @@ export default async function Petitions() {
 					width: "100vw",
 					height: "70vh",
 				}}>
-				<div className="absolute inset-0 mx-auto w-fit md:mt-32 lg:mt-60" data-aos="fade-down">
-					<div className="flex flex-col justify-center rounded-xl bg-white p-20">
+				<div className="absolute inset-0 mx-auto mt-28 w-fit md:mt-32 lg:mt-60" data-aos="fade-down">
+					<div className="flex flex-col justify-center rounded-xl bg-white p-10 lg:p-20">
 						<h1 className="text-center text-6xl text-blue-600">Petitions</h1>
 						<h2 className="text-wrap pt-6 text-center text-xl text-green-600">
 							Be the change you want to see in the world.
@@ -33,23 +33,23 @@ export default async function Petitions() {
 				</div>
 			</div>
 			<div
-				className="relative flex h-full w-full bg-fixed"
+				className="relative flex w-full bg-fixed"
 				style={{
 					backgroundImage:
 						"url(https://static.wixstatic.com/media/11062b_c2b35eecf8974f979019c6830b0b4964~mv2.jpg/v1/fill/w_1587,h_810,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_c2b35eecf8974f979019c6830b0b4964~mv2.jpg)",
 					backgroundSize: "cover",
-					backgroundRepeat: "no-repeat",
+					backgroundRepeat: "repeat",
 					width: "100vw",
-					height: "110vh",
+					minHeight: "110vh",
 				}}>
-				<div className="absolute inset-0 mx-auto w-10/12 md:mt-32 lg:mt-32" data-aos="fade-down">
-					<div className="flex justify-center gap-6">
+				<div className="absolute inset-0 mx-auto my-16 w-10/12 pb-8 xl:mt-28" data-aos="fade-down">
+					<div className="flex flex-col justify-center gap-6 xl:flex-row">
 						<div className="basis-3/5">
-							<div className="h-fit rounded-xl bg-white">
+							<div className="h-fit rounded-xl bg-white xl:py-4">
 								<h1 className="pt-8 text-center text-3xl font-semibold text-blue-500">
 									The Flint Water Crisis - Holding the EPA Accountable
 								</h1>
-								<h2 className="mx-10 py-6 text-green-700">
+								<h2 className="mx-4 py-6 text-sm text-green-700 md:mx-6 md:text-sm lg:mx-8 lg:text-base">
 									The water catastrophe is expanding rapidly and inevitably across the United States, threatening the
 									health of millions of people and calling into question the diligence of environmental organizations
 									such as the Environmental Protection Agency. The Flint Water Crisis began in 2014 when the city of
@@ -81,40 +81,42 @@ export default async function Petitions() {
 						</div>
 						<div className="basis-2/5">
 							<div className="rounded-2xl bg-white">
-								<div className="mx-4 py-8">
-									<h1 className="py-6 text-5xl text-green-700">Sign Today!</h1>
-									<h2 className="text-lg text-blue-500">Ensure ALL Americans have access to clean water.</h2>
+								<div className="mx-4 py-4 lg:py-8">
+									<h1 className="text-wrap py-6 text-center text-5xl text-green-700 lg:text-left">Sign Today!</h1>
+									<h2 className="text-wrap text-center text-lg text-blue-500 lg:text-left">
+										Ensure ALL Americans have access to clean water.
+									</h2>
 								</div>
-								<div className="mx-6 grid grid-cols-2 grid-rows-3 gap-3">
-									<div className="col-span-1 row-span-1 flex-col">
+								<div className="mx-6 grid justify-center gap-3 md:flex-col lg:grid-cols-2 lg:grid-rows-3">
+									<div className="flex-col lg:col-span-1 lg:row-span-1">
 										<p className="text-blue-400">First Name *</p>
 										<input
-											className="border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
+											className="w-full border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
 											type="text"
 										/>
 									</div>
-									<div className="col-span-1 col-start-2 row-span-1 flex-col">
+									<div className="flex-col lg:col-span-1 lg:col-start-2 lg:row-span-1">
 										<p className="text-blue-400">Last Name *</p>
 										<input
-											className="border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
+											className="w-full border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
 											type="text"
 										/>
 									</div>
-									<div className="col-span-1 col-start-1 row-span-1 flex-col">
+									<div className="flex-col lg:col-span-1 lg:col-start-1 lg:row-span-1">
 										<p className="text-blue-400">Email *</p>
 										<input
-											className="border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
+											className="w-full border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
 											type="email"
 										/>
 									</div>
-									<div className="col-span-1 col-start-1 row-span-1 flex-col">
+									<div className="flex-col lg:col-span-1 lg:col-start-1 lg:row-span-1">
 										<p className="text-blue-400">Zip Code *</p>
-										<input className="border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white" />
+										<input className="w-full border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white" />
 									</div>
 									<div className="col-span-1 col-start-2 row-span-2 row-start-2 flex-col">
 										<p className="text-blue-400">Message</p>
 										<textarea
-											className="h-fit resize-none border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
+											className="h-fit w-full resize-none border-b-2 border-b-blue-900 bg-blue-400 p-3 duration-200 hover:bg-white focus:bg-white"
 											rows={5}
 										/>
 									</div>
