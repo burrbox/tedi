a2d01270af9718135efea9dd15c71cad6d1863d7
Nash Pillai <nash.pillai@protonmail.com>
2024-07-15 22:45:31 -0400

    fix loading google proflie pictures

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
 	images: {
-		domains: ["cdn.discordapp.com"],
+		domains: ["cdn.discordapp.com", "lh3.googleusercontent.com"],
 	},
 };
 
