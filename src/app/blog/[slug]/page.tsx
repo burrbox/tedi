435366f8544f4e496829b2b2f591052323c806f3
HiroDookie <hryk.sakuma@gmail.com>
2024-07-18 16:33:02 +0900

    footer for individual blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -35,7 +35,7 @@ export default async function SinglePost({ params }: { params: { slug: string } 
 
 	return (
 		<>
-			<section className="relative">
+			<section className="relative dark:bg-stone-900">
 				{/* Background image */}
 				{post.image && (
 					<div className="h-128 absolute inset-0 box-content pt-16">
@@ -51,7 +51,7 @@ export default async function SinglePost({ params }: { params: { slug: string } 
 					</div>
 				)}
 
-				<div className="relative mx-auto max-w-6xl px-4 sm:px-6">
+				<div className="relative mx-auto max-w-6xl px-4 sm:px-6 dark:bg-stone-900">
 					<div className="pb-12 pt-32 md:pb-20 md:pt-40">
 						<div className="mx-auto max-w-3xl">
 							<article>
@@ -59,10 +59,10 @@ export default async function SinglePost({ params }: { params: { slug: string } 
 								<header className="mb-8">
 									{/* Title and excerpt */}
 									<div className="text-center md:text-left">
-										<h1 className="h1 font-red-hat-display mb-4" data-aos="fade-down">
+										<h1 className="h1 font-red-hat-display mb-4 dark:text-green-500" data-aos="fade-down">
 											{post.title}
 										</h1>
-										<p className="text-xl text-gray-600 dark:text-gray-400" data-aos="fade-down" data-aos-delay="150">
+										<p className="text-xl text-gray-600 dark:text-stone-300" data-aos="fade-down" data-aos-delay="150">
 											{post.summary}
 										</p>
 									</div>
@@ -80,11 +80,11 @@ export default async function SinglePost({ params }: { params: { slug: string } 
 												/>
 											</a>
 											<div>
-												<span className="text-gray-600 dark:text-gray-400">By </span>
-												<a className="font-medium text-gray-800 hover:underline dark:text-gray-300" href="#0">
+												<span className="text-gray-600 dark:text-blue-400">By </span>
+												<a className="font-medium text-gray-800 hover:underline dark:text-blue-300" href="#0">
 													{author.name ?? "Anonymous Author"}
 												</a>
-												<span className="text-gray-600 dark:text-gray-400">
+												<span className="text-gray-600 dark:text-blue-400">
 													{" "}
 													{/* · <PostDate dateString={post.createdAt} /> */}
 												</span>
@@ -93,7 +93,7 @@ export default async function SinglePost({ params }: { params: { slug: string } 
 									</div>
 								</header>
 								<hr
-									className="mb-8 h-px w-5 border-0 bg-gray-400 pt-px dark:bg-gray-500"
+									className="mb-8 h-px w-5 border-0 bg-gray-400 pt-px dark:bg-white"
 									data-aos="fade-down"
 									data-aos-delay="450"
 								/>
@@ -106,6 +106,45 @@ export default async function SinglePost({ params }: { params: { slug: string } 
 									<Mdx content={post.content} />
 								</div>
 							</article>
+							<div className="dark:bg-stone-900">
+								<div className="mx-auto flex flex-col justify-center">
+									<h3 className="text-wrap text-blue-500 dark:text-blue-400">
+										Stay updated and active by following the Environmental Defense Initiative on Medium and all our
+										social media platforms!
+									</h3>
+									<div className="py-6 text-black dark:text-white">
+										<ul className="mb-4 flex gap-2 md:order-2 md:mb-0 md:ml-4 md:gap-4">
+											<button className="rounded-xl bg-green-700 px-5 py-2">
+												<label>
+													<a href="https://www.instagram.com/environmentaldefenseinitiative/">Instagram</a>
+												</label>
+											</button>
+											<button className="rounded-xl bg-green-700 px-5 py-2">
+												<label>
+													<a href="https://medium.com/@environmentaldefenseinitiative">Medium</a>
+												</label>
+											</button>
+											<button className="rounded-xl bg-green-700 px-5 py-2">
+												<label>
+													<a href="https://www.youtube.com/channel/UCLJIczzKZWeqsa2Pmg55F6g">YouTube</a>
+												</label>
+											</button>
+											<button className="rounded-xl bg-green-700 px-5 py-2">
+												<label>
+													<a href="https://www.tiktok.com/@tediactivism">TikTok</a>
+												</label>
+											</button>
+										</ul>
+									</div>
+									<div>
+										<p>
+											Author: <span>{post.author}</span>
+											<br />
+											Editor: <span>{post.editor}</span>
+										</p>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
