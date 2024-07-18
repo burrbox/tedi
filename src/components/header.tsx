39a5e946caa7cce96198f6eaca5490b6a1d43784
Nash Pillai <nash.pillai@protonmail.com>
2024-07-18 00:44:21 -0400

    add sign out button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { CldImage } from "next-cloudinary";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const links = [
 	// { name: "Home", href: "/" },
@@ -27,7 +35,7 @@ export default function Header() {
 			<div className="container mx-auto flex items-center justify-between px-4 py-4">
 				<Link href={"/"} className="flex items-center space-x-2">
 					<CldImage width="40" height="40" src="logo" alt="Logo" className="h-10 w-10" />
-					<h1 className="hidden text-xl font-bold text-green-700 dark:text-blue-400 sm:block">
+					<h1 className="hidden text-xl font-bold text-green-700 sm:block dark:text-blue-400">
 						The Environmental Defense Initiative
 					</h1>
 				</Link>
@@ -43,23 +51,36 @@ export default function Header() {
 						</Link>
 					))}
 					{session?.user ? (
-						<div className="border-gray-700 pb-3 pt-4">
-							<div className="flex items-center px-5">
-								<div className="mr-3 flex-shrink-0">
-									<Image
-										className="h-10 w-10 rounded-full"
-										src={session?.user.image ?? "https://img.icons8.com/96/FFFFFF/user-male-circle.png"}
-										width={40}
-										height={40}
-										alt=""
-									/>
+						<DropdownMenu>
+							<DropdownMenuTrigger>
+								<div className="border-gray-700 pb-3 pt-4">
+									<div className="flex items-center px-5">
+										<div className="mr-3 flex-shrink-0">
+											<Image
+												className="h-10 w-10 rounded-full"
+												src={session.user.image ?? "https://res.cloudinary.com/mozzarella-tedi/image/upload/utter.png"}
+												width={40}
+												height={40}
+												alt=""
+											/>
+										</div>
+										<div className="hidden lg:block">
+											<div className="text-base font-medium text-gray-800 dark:text-white">{session.user.name}</div>
+											<div className="text-sm font-medium text-gray-600 dark:text-gray-300">{session.user.email}</div>
+										</div>
+									</div>
 								</div>
-								<div className="hidden lg:block">
-									<div className="text-base font-medium text-gray-800 dark:text-white">{session?.user.name}</div>
-									<div className="text-sm font-medium text-gray-600 dark:text-gray-300">{session?.user.email}</div>
-								</div>
-							</div>
-						</div>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<DropdownMenuLabel>My Account</DropdownMenuLabel>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem>Profile</DropdownMenuItem>
+								<DropdownMenuItem>Settings</DropdownMenuItem>
+								<DropdownMenuItem>
+									<button onClick={() => signOut()}>Sign Out</button>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
 					) : (
 						<Link className="btn-sm group w-full bg-blue-700 shadow-sm md:w-max" href={`/signin?redirect=${path}`}>
 							{/* <ShimmerButton className="shadow-2xl" background="#4F46E5"> */}
