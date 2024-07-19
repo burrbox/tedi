870116957ae15e09d20969e57041b55cc6b44286
HiroDookie <hryk.sakuma@gmail.com>
2024-07-19 12:34:54 +0900

    replace images on petitions

--- src/app/petitions/page.tsx
+++ src/app/petitions/page.tsx
@@ -1,6 +1,5 @@
 import { petition } from "@/lib/constants";
 import PetitionForm from "./petitionForm";
-import Head from "next/head";
 import { CloudinaryClientWrapper } from "@/components/cloudinaryClientWrapper";
 import { ImageResponse } from "next/og";
 
@@ -10,22 +9,26 @@ export const metadata = {
 };
 
 export default async function Petitions({}) {
-	<Head>
+	<head>
 		<title>Petitions - TEDI</title>
 		<meta
 			property="og:image"
-			content="https://static.wixstatic.com/media/7cc6b0344c9440818bad598dd590ed27.jpg/v1/fill/w_1587,h_654,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/7cc6b0344c9440818bad598dd590ed27.jpg"
+			content="https://res.cloudinary.com/mozzarella-tedi/image/upload/f_auto,q_auto/v1/nature/tallForest"
 		/>
+		<meta property="og:image:width" content="1200" />
+		<meta property="og:image:height" content="630" />
+		<meta property="og:type" content="Blog home page" />
+		<meta property="og:url" content="https://www.tedinitative.org/blog/" />
 		<meta property="og:description" content="Sign a petition to advocate for new policies." />
-	</Head>;
+	</head>;
 
 	return (
 		<section className="h-full w-full flex-col items-end">
 			<div
-				title="An background image of a butterfly"
+				title="An image of the Redwood Forest"
 				style={{
 					backgroundImage:
-						"url(https://static.wixstatic.com/media/7cc6b0344c9440818bad598dd590ed27.jpg/v1/fill/w_1587,h_654,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/7cc6b0344c9440818bad598dd590ed27.jpg)",
+						"url(https://res.cloudinary.com/mozzarella-tedi/image/upload/f_auto,q_auto/v1/nature/tallForest)",
 				}}
 				className="flex h-[70vh] w-screen bg-cover bg-fixed bg-center bg-no-repeat">
 				<div className="container relative mx-auto h-fit w-full px-4 py-16">
@@ -46,23 +49,25 @@ export default async function Petitions({}) {
 					<span>
 						<img
 							alt="An image of a protest. One of the protesters is holding a sign that reads 'The Climate is Chagning. Why aren't we?'."
-							src="https://static.wixstatic.com/media/11062b_658fbb9afab94e49b67099039406c25a~mv2.jpeg/v1/fill/w_600,h_400,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_658fbb9afab94e49b67099039406c25a~mv2.jpeg"
+							src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpeoplesdispatch.org%2Fwp-content%2Fuploads%2F2019%2F03%2Fglobal-strike-4-climate.jpg&f=1&nofb=1&ipt=dcb808b33e4a67306edfd9698dc60df55f4bc148211da879e84eb43727088751&ipo=images"
+							className="h-96 object-cover"
 						/>
 					</span>
 					<span>
 						<img
 							alt="An image of a protest. One of the protesters is holding a sign of the world on fire and read 'WAKE UP'."
-							src="https://static.wixstatic.com/media/11062b_d4083c8315294d4ba93cfdc700edb547~mv2_d_3840_5760_s_4_2.jpg/v1/crop/x_0,y_1691,w_3840,h_4059/fill/w_379,h_401,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/11062b_d4083c8315294d4ba93cfdc700edb547~mv2_d_3840_5760_s_4_2.jpg"
+							src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.V-oo5LTTojTNnhLqkqNYQwHaE8%26pid%3DApi&f=1&ipt=3e5984c18fcfe9f3f90f5ce7118ab302ef61af3ab8fbbe8d0933ab2b04929de7&ipo=images"
+							className="h-96 object-cover"
 						/>
 					</span>
 				</div>
 			</div>
 			<div
 				className="relative flex min-h-[110vh] w-screen bg-cover bg-fixed bg-center bg-repeat py-16 xl:pt-28"
-				title="An image of water droplets"
+				title="An image of a beach and ocean"
 				style={{
 					backgroundImage:
-						"url(https://static.wixstatic.com/media/11062b_c2b35eecf8974f979019c6830b0b4964~mv2.jpg/v1/fill/w_1587,h_810,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/11062b_c2b35eecf8974f979019c6830b0b4964~mv2.jpg)",
+						"url(https://res.cloudinary.com/mozzarella-tedi/image/upload/f_auto,q_auto/v1/nature/t7x9xmnsyqs2yetfpp3j)",
 				}}>
 				<div className="inset-0 mx-auto w-full sm:w-10/12" data-aos="fade-down">
 					<div className="flex flex-col justify-center gap-6 xl:flex-row">
