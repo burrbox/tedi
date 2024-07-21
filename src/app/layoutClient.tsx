af53b767c7c0216d546e92cd3f186556f3d2e3f6
Nash Pillai <nash.pillai@protonmail.com>
2024-07-21 01:33:25 -0400

    prompt people to sign petition with a toast

--- src/app/layoutClient.tsx
+++ src/app/layoutClient.tsx
@@ -8,6 +8,10 @@ import "aos/dist/aos.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { useEffect } from "react";
+import { wait } from "@/lib/utils";
+import { toast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+import Link from "next/link";
 
 export default function RootLayoutClient({ children }: { children: React.ReactNode }) {
 	useEffect(() => {
@@ -18,6 +22,21 @@ export default function RootLayoutClient({ children }: { children: React.ReactNo
 			easing: "ease-out-sine",
 		});
 	});
+
+	useEffect(() => {
+		void wait(3000).then(() => {
+			toast({
+				title: "Would you like to sign a petition?",
+				description: "Fight for the environment!",
+				duration: 5000,
+				action: (
+					<ToastAction altText="Sign">
+						<Link href="/petitions">Sign</Link>
+					</ToastAction>
+				),
+			});
+		});
+	}, []);
 
 	return (
 		<SessionProvider>
