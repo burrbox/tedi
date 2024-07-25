45ac3863ea7b4f71185362fe6861d227b2c4d06c
Nash Pillai <nash.pillai@protonmail.com>
2024-07-25 00:39:43 -0400

    significantly improve image and caption formatting

--- src/components/mdx/link.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function PostLink({ href, ...props }: { href?: string; children?: React.ReactNode }) {
-	return (
-		<Link href={href ?? "/"} {...props}>
-			{props.children}
-		</Link>
-	);
-}
