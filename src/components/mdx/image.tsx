0cf92f6d34445362a90eb9e17f799abba6b34090
Nash Pillai <nash.pillai@protonmail.com>
2024-07-25 00:09:41 -0400

    fix image captions by unwrapping images in paragraphs

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -12,11 +12,12 @@ export default function PostImage({
 }) {
 	// console.log("PostImage:", { alt, caption, align, src, ...props });
 	const classes = align === "left" ? "md:float-left md:max-w-sm md:mr-8 md:mt-0 lg:max-w-none lg:-ml-32" : "";
+	caption = caption ?? alt;
 
 	return (
 		<figure className={classes}>
 			<img className="w-full" {...props} src={src} alt={alt} />
-			{caption && <figcaption className="mt-3 text-center text-sm text-gray-500">{alt}</figcaption>}
+			{caption && <figcaption className="mt-3 text-center text-sm text-gray-500">{caption}</figcaption>}
 		</figure>
 	);
 }
--- src/components/mdx/mdx.tsx
+++ src/components/mdx/mdx.tsx
@@ -2,6 +2,7 @@
 
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import remarkUnwrapImages from "remark-unwrap-images";
 import rehypeRaw from "rehype-raw";
 import { type PluggableList } from "node_modules/react-markdown/lib";
 import PostLink from "./link";
@@ -12,7 +13,7 @@ export function Mdx({ content }: { content: string }) {
 		// <article className="prose prose-lg prose-headings:text-gray-900 prose-p:leading-normal prose-a:font-medium prose-a:text-teal-500 prose-a:no-underline hover:prose-a:underline prose-blockquote:border-l-2 prose-blockquote:border-teal-500 prose-blockquote:pl-4 prose-blockquote:font-normal prose-blockquote:italic prose-blockquote:text-inherit prose-strong:font-medium prose-strong:text-gray-900 dark:prose-headings:text-gray-100 dark:prose-blockquote:border-gray-400 dark:prose-strong:text-gray-100 max-w-none text-lg text-gray-600 dark:text-gray-400">
 		<div className="prose mx-auto overflow-y-hidden p-2 text-left leading-normal dark:prose-invert prose-h1:text-2xl prose-a:text-blue-600 prose-a:underline prose-td:border-2 prose-td:border-gray-500 prose-td:p-2 prose-img:mx-auto prose-img:my-0 prose-img:w-1/2 dark:prose-td:border-white">
 			<ReactMarkdown
-				remarkPlugins={[remarkGfm] as PluggableList}
+				remarkPlugins={[remarkGfm, remarkUnwrapImages] as PluggableList}
 				rehypePlugins={[rehypeRaw]}
 				components={{
 					a: PostLink,
