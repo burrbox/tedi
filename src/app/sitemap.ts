1a14254069b6f4d0918a6ba31354dc1588f58817
Nash Pillai <nash.pillai@protonmail.com>
2024-07-27 10:54:04 -0400

    update sitemap with /error and /our-work

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,36 +6,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const posts = await getPosts();
 	return (
 		[
-			{
-				url: `${env.URL}`,
-				lastModified: new Date(),
-				changeFrequency: "monthly",
-				priority: 1,
-			},
-			{
-				url: `${env.URL}/about`,
-				lastModified: new Date(),
-				changeFrequency: "monthly",
-				priority: 0.8,
-			},
-			{
-				url: `${env.URL}/petitions`,
-				lastModified: new Date(),
-				changeFrequency: "weekly",
-				priority: 1,
-			},
-			{
-				url: `${env.URL}/signin`,
-				lastModified: new Date(),
-				changeFrequency: "yearly",
-				priority: 0.1,
-			},
-			{
-				url: `${env.URL}/blog`,
-				lastModified: new Date(),
-				changeFrequency: "weekly",
-				priority: 1,
-			},
+			{ url: env.URL, lastModified: new Date(), changeFrequency: "monthly", priority: 1 },
+			{ url: `${env.URL}/about`, lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
+			{ url: `${env.URL}/petitions`, lastModified: new Date(), changeFrequency: "weekly", priority: 1 },
+			{ url: `${env.URL}/our-work`, lastModified: new Date(), changeFrequency: "monthly", priority: 0.8 },
+			{ url: `${env.URL}/signin`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.1 },
+			{ url: `${env.URL}/error`, lastModified: new Date(), changeFrequency: "yearly", priority: 0.1 },
+			{ url: `${env.URL}/blog`, lastModified: new Date(), changeFrequency: "weekly", priority: 1 },
 		] as MetadataRoute.Sitemap
 	).concat(
 		posts.map((post) => ({
