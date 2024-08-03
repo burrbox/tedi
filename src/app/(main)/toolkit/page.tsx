1292d48ccf5f18eab90fa301436d87f6fc703921
Nash Pillai <nash.pillai@protonmail.com>
2024-08-03 03:20:03 -0400

    add globe to toolkit page with customizable speed

---
+++ src/app/(main)/toolkit/page.tsx
@@ -0,0 +1,58 @@
+import { type Metadata } from "next";
+import { env } from "@/env";
+import { getCldOgImageUrl } from "next-cloudinary";
+import Globe from "@/components/magicui/globe";
+import { type COBEOptions } from "cobe";
+
+export const metadata: Metadata = {
+	title: "Toolkits",
+	description: "Our advocacy toolkits.",
+	alternates: { canonical: `${env.URL}/petitions` },
+	openGraph: {
+		siteName: "The Environmental Defense Initiative",
+		url: `${env.URL}/petitions`,
+		title: "Petitions - TEDI",
+		type: "website",
+		description: "Our advocacy toolkits.",
+		images: {
+			url: getCldOgImageUrl({ src: "nature/tallForest" }),
+			width: 1200,
+			height: 627,
+			alt: "An image of the Redwood Forest",
+		},
+	},
+};
+
+const globeConfig: Partial<COBEOptions> = {
+	phi: 0.02,
+	dark: 1,
+	diffuse: 0.4,
+	mapSamples: 16000,
+	mapBrightness: 1.2,
+	baseColor: [1, 1, 1],
+	markerColor: [251 / 255, 100 / 255, 21 / 255],
+	glowColor: [1, 1, 1],
+	markers: [
+		{ location: [14.5995, 120.9842], size: 0.03 },
+		{ location: [19.076, 72.8777], size: 0.1 },
+		{ location: [23.8103, 90.4125], size: 0.05 },
+		{ location: [30.0444, 31.2357], size: 0.07 },
+		{ location: [39.9042, 116.4074], size: 0.08 },
+		{ location: [-23.5505, -46.6333], size: 0.1 },
+		{ location: [19.4326, -99.1332], size: 0.1 },
+		{ location: [40.7128, -74.006], size: 0.1 },
+		{ location: [34.6937, 135.5022], size: 0.05 },
+		{ location: [41.0082, 28.9784], size: 0.06 },
+	],
+};
+
+export default async function Toolkit() {
+	return (
+		<div className="h-full w-full">
+			<section className="pt-8">
+				<h1 className="text-center text-4xl font-bold text-gray-900 dark:text-gray-100">See where our members are!</h1>
+				<Globe className="mt-24 w-full" config={globeConfig} speed={0.001} />
+			</section>
+		</div>
+	);
+}
