68241ab0f541868cdd4763cab2c8ffc30cd09bdb
Hiro <hryk.sakuma@gmail.com>
2024-10-25 22:46:42 -0400

    toolkits updated

--- src/app/(main)/toolkit/page.tsx
+++ src/app/(main)/toolkit/page.tsx
@@ -86,7 +86,7 @@ export default async function Toolkit() {
 							</p>
 							<div className="flex">
 								<Link
-									className="rounded-lg bg-green-500 px-5 py-3 text-center duration-300 hover:bg-green-400 hover:px-7 dark:bg-green-600 dark:hover:bg-green-700"
+									className="rounded-lg bg-green-500 px-5 py-3 text-center duration-300 hover:scale-110 hover:bg-green-400 dark:bg-green-600 dark:hover:bg-green-700"
 									href={`https://www.canva.com/design/DAGNVCdAnYI/YQpmkVQrIaY_P0K3t_NXUA/view`}
 									target="_blank"
 								>
@@ -130,8 +130,105 @@ export default async function Toolkit() {
 							</p>
 							<div className="flex">
 								<Link
-									className="rounded-lg bg-green-500 px-5 py-3 text-center duration-300 hover:bg-green-400 hover:px-7 dark:bg-green-600 dark:hover:bg-green-700"
+									className="rounded-lg bg-green-500 px-5 py-3 text-center duration-300 hover:scale-110 hover:bg-green-400 dark:bg-green-600 dark:hover:bg-green-700"
 									href={` https://www.canva.com/design/DAGQCx9CWp0/9FyW8cBqiksP8KVfh-HtLg/edit?utm_content=DAGQCx9CWp0&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton`}
+									target="_blank"
+								>
+									Download
+								</Link>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+			<div className="w-full bg-stone-100 px-8 py-10 dark:bg-stone-800">
+				<section className="mx-auto mt-8 max-w-7xl">
+					<div className="flex flex-col-reverse gap-8 sm:flex-row">
+						<div>
+							<CloudinaryClientWrapper
+								className="max-h-[80vh] w-fit rounded-xl"
+								src="toolkit/catzc1tx5w9rfkrri1pe"
+								alt="The toolkit for the cleanup campaign"
+								width={1080}
+								height={2160}
+							/>
+						</div>
+						<div className="w-4/5 px-10">
+							<h1 className="mt-2 w-full text-wrap text-6xl font-bold text-green-500">
+								The Sustainable Crafts Toolkit
+							</h1>
+							<hr className="mt-5 w-3/4 border-green-500" />
+							<p className="my-10 text-xl">
+								<span className="text-lg">
+									Are you an environmentally conscious arts and crafts person? <br />
+									Learn about some cool sustainable crafts here!
+								</span>
+								<br />
+								<br />
+								Unlock your creativity with our{" "}
+								<span className="text-blue-600 dark:text-blue-400">sustainable crafts</span> toolkit! Designed for
+								children of all ages, this kit offers{" "}
+								<span className="text-blue-600 dark:text-blue-400">quick and easy sustainable crafts</span> that teach
+								kids the importance of environmental stewardship. Each project is{" "}
+								<span className="text-blue-600 dark:text-blue-400">
+									fun, educational, and crafted from eco-friendly materials
+								</span>
+								, making it simple for children to learn about sustainability while making unique, earth-friendly
+								creations!
+							</p>
+							<div className="flex">
+								<Link
+									className="rounded-lg bg-green-500 px-5 py-3 text-center duration-300 hover:scale-110 hover:bg-green-400 dark:bg-green-600 dark:hover:bg-green-700"
+									href={`https://www.canva.com/design/DAGPpNZtVdc/D6i7ps-BuymyeECKzqi7xw/edit?utm_content=DAGPpNZtVdc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton`}
+									target="_blank"
+								>
+									Download
+								</Link>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+			<div className="w-full px-8 py-10">
+				<section className="mx-auto mt-8 max-w-7xl">
+					<div className="flex flex-col-reverse gap-8 sm:flex-row">
+						<div>
+							<CloudinaryClientWrapper
+								className="max-h-[80vh] w-fit rounded-xl"
+								src="toolkit/mypiky7xtlgv6jrx398a"
+								alt="The toolkit for the cleanup campaign"
+								width={1080}
+								height={2160}
+							/>
+						</div>
+						<div className="w-4/5 px-10">
+							<h1 className="mt-2 w-full text-wrap text-6xl font-bold text-green-500">
+								Engaging Kids in Environmental Awareness
+							</h1>
+							<hr className="mt-5 w-3/4 border-green-500" />
+							<p className="my-10 text-xl">
+								<span className="text-lg">
+									Interested in spreading environmental awareness to a younger audience? <br />
+									Our toolkit is can help you engage kids and teach them about the environment!
+								</span>
+								<br />
+								<br />
+								Empower the next generation with our{" "}
+								<span className="text-blue-600 dark:text-blue-400">Engaging Kids in Environmental Awareness</span>{" "}
+								toolkit! This comprehensive resource equips educators and parents with{" "}
+								<span className="text-blue-600 dark:text-blue-400">
+									engaging games, interactive lessons, and hands-on activities
+								</span>{" "}
+								designed to teach children of all ages the fundamentals of environmental stewardship. From fun,
+								age-appropriate projects to insightful discussions, this toolkit makes it easy to introduce kids to the
+								essentials of <span className="text-blue-600 dark:text-blue-400">caring for our planet</span> while
+								sparking their curiosity and passion for a{" "}
+								<span className="text-blue-600 dark:text-blue-400">sustainable future</span>.
+							</p>
+							<div className="flex">
+								<Link
+									className="rounded-lg bg-green-500 px-5 py-3 text-center duration-300 hover:scale-110 hover:bg-green-400 dark:bg-green-600 dark:hover:bg-green-700"
+									href={` https://www.canva.com/design/DAGPpuL1_ts/5Dcos8JEWNvBtO9pxKs4zw/edit?utm_content=DAGPpuL1_ts&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton`}
 									target="_blank"
 								>
 									Download
