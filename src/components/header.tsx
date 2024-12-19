417faf6a908d52c2e5aa93a5f23fb33027db6bc6
Nash Pillai <nash.pillai@protonmail.com>
2024-12-18 19:55:43 -0500

    add back donate button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -16,6 +16,7 @@ import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
 import { Package2 } from "lucide-react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import { stripeDonation } from "@/lib/serverActions";
 
 const links = [
 	// { name: "Home", href: "/" },
@@ -80,13 +81,14 @@ export default function Header() {
 							{link.name}
 						</Link>
 					))}
-					{/*<button
+					<button
 						className="group inline-flex items-center justify-center rounded bg-green-500 px-2 py-1 leading-snug shadow-sm sm:px-4 sm:py-2 md:w-max dark:bg-green-700"
-						onClick={() => stripeDonation()}>
+						onClick={() => stripeDonation()}
+					>
 						<span className="whitespace-pre-wrap from-white to-slate-900/10 text-center text-lg font-medium leading-none tracking-tight text-white">
 							Donate
 						</span>
-					</button> */}
+					</button>
 					{session?.user ? (
 						<DropdownMenu>
 							<DropdownMenuTrigger>
